52f94bfb748c7f8b9789b3e3d5369f4447f6f365
daniaalrababa9 <rababadania@gmail.com>
2020-01-27 14:56:29 +0200

    add fs file

---
+++ edit-file.js
@@ -0,0 +1,24 @@
+'use strict';
+const fs=require('fs');
+const util =require('util');
+let file = `${__dirname}/categories.db`;
+
+let readFile =util.promisify(fs.readFile);
+readFile(file)
+    .then(data=>{
+        return JSON.parse(data)
+    })
+    .then(data=>writerWithPromise(file,data))
+    .catch(error=>error)
+
+
+
+const writeFile =util.promisify(fs.writeFile);
+
+const writerWithPromise=(file,data)=>{
+    data.firstName='dania';
+    let dataAfterModifing=JSON.stringify(data);
+    writeFile(file,dataAfterModifing)
+}
+
+module.exports={readFile,writerWithPromise}
